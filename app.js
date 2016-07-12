3d13b0a7dd1cbc1a55bb85f8873d217e60c8ce66
Miguel Coquet <miguel@mcoquet.com>
2016-07-12 18:01:05 +0100

    added vorpal, added act and connect commands

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("meow!");
+var Vorpal = require("vorpal")();
+var Seneca = require("seneca")();
+
+var Client = false;
+
+Vorpal.delimiter("seneca explorer > ").show();
+
+Vorpal.command("act <action>","send out an action string and get the return result printed out.").action(function(args,callback){
+  Vorpal.log("sending out",args.action);
+  if (Client) {
+    Client.act(args.action,function(err,response){
+      Vorpal.log("...");
+      callback(JSON.stringify(response));
+    })
+  }
+  else {
+    callback("Not connected. Use 'connect'");
+  }
+});
+
+Vorpal.command("connect [host] [port]","connect to a seneca instance").action(function (args,callback) {
+
+  var options = {
+    host: args.host || "localhost",
+    port: args.port || 3000
+  }
+
+  Client = Seneca.client( options );
+  callback();
+});
+
+Seneca.ready(function(){
+  Vorpal.log("Seneca is ready.");
+});
